bc6c02f72ac68614cfc93116d88edd9026208703
ZoeLi1997 <zli32@calpoly.edu>
2019-10-29 18:36:21 -0700

    last commit

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -7,72 +7,72 @@ Vue.use(Vuex);
 
 const vuexPersist = new VuexPersist({
     key: "project-template",
-    storage: window.localStorage
-)};
+    storage: window.localStorage,
+});
 
 export const mutations = {
-  login: function(state) {
-    state.loginState = { ...state.loginState, loggedIn: true };
-  },
-  logout: function(state) {
-    state.loginState = { ...state.loginState, loggedIn: false };
-  },
-   addToDo(state, todo) {
-            debugger;
-            state.todos = [...state.todos, {...todo, done: false, id: state.todos.length + 1 }];
-        },
-        markToDo(state, todo) {
-            debugger;
-            for (let i = 0; i < state.todos.length; i++) {
-                if (state.todos[i] === todo) {
-                    state.todos[i].done = !todo.done
-                }
-            }
-        },
-        deleteToDo(state, todo) {
-            debugger;
-            // state.todos[todo.id].done = !todo.done
-            for (let i = 0; i < state.todos.length; i++) {
-                if (state.todos[i] === todo) {
-                    state.todos.splice(i, 1)
-                }
+    login: function(state) {
+        state.loginState = {...state.loginState, loggedIn: true };
+    },
+    logout: function(state) {
+        state.loginState = {...state.loginState, loggedIn: false };
+    },
+    addToDo(state, todo) {
+        debugger;
+        state.todos = [...state.todos, {...todo, done: false, id: state.todos.length + 1 }];
+    },
+    markToDo(state, todo) {
+        debugger;
+        for (let i = 0; i < state.todos.length; i++) {
+            if (state.todos[i] === todo) {
+                state.todos[i].done = !todo.done
             }
         }
+    },
+    deleteToDo(state, todo) {
+        debugger;
+        // state.todos[todo.id].done = !todo.done
+        for (let i = 0; i < state.todos.length; i++) {
+            if (state.todos[i] === todo) {
+                state.todos.splice(i, 1)
+            }
+        }
+    }
 };
 
 export const actions = {
-  login: function({ commit }, payload) {
-    const { email, password } = payload;
-    return axios.post("/api/login", { email, password }).then(() => {
-      commit("login");
-    });
-  },
-  logout: function({ commit }) {
-    return axios.get("/api/logout").then(() => {
-      commit("logout");
-    });
-  },
-  addToDo({ commit }, toDo) {
-            debugger;
-            commit("addToDo", toDo);
-        },
-        markToDo({ commit }, toDo) {
-            debugger;
-            commit("markToDo", toDo);
-        },
-        deleteToDo({ commit }, toDo) {
-            debugger;
-            commit("deleteToDo", toDo);
-        }
+    login: function({ commit }, payload) {
+        const { email, password } = payload;
+        return axios.post("/api/login", { email, password }).then(() => {
+            commit("login");
+        });
+    },
+    logout: function({ commit }) {
+        return axios.get("/api/logout").then(() => {
+            commit("logout");
+        });
+    },
+    addToDo({ commit }, toDo) {
+        debugger;
+        commit("addToDo", toDo);
+    },
+    markToDo({ commit }, toDo) {
+        debugger;
+        commit("markToDo", toDo);
+    },
+    deleteToDo({ commit }, toDo) {
+        debugger;
+        commit("deleteToDo", toDo);
+    }
 };
 
 export default new Vuex.Store({
-  plugins: [vuexPersist.plugin],
-  state: {
-    loginState: {
-      loggedIn: false
-    }
-  },
-  mutations,
-  actions
+    plugins: [vuexPersist.plugin],
+    state: {
+        loginState: {
+            loggedIn: false
+        }
+    },
+    mutations,
+    actions
 });
