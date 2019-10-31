dbb81137a6f878a8f1313ed092d7d1efb9ec35d5
Jon Prentice <jon77p@gmail.com>
2019-10-31 10:09:35 -0700

    Completed API implementationof lab

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -2,29 +2,37 @@ import { Router } from 'express';
 import { getRepository, getManager } from 'typeorm';
 import isAuthenticated from '../middleware/isAuthenticated';
 import ToDo from '../entities/todo';
+import Category from '../entities/category';
 
 const router = Router();
 router.route('/todos')
   .all(isAuthenticated)
   .get((req, res) => {
-    getRepository(ToDo).find({ where: { userId: req.user.id } }).then((todos) => {
+    getRepository(ToDo).find({ where: { userId: req.user.id }, relations: ['category'] }).then((todos) => {
       res.send(todos);
     });
   })
   .post((req, res) => {
-    const { done, title } = req.body;
-    const manager = getManager();
-    const todo = manager.create(ToDo, { done, title });
-    todo.user = req.user;
-    manager.save(todo).then((savedTodo) => {
-      res.send(savedTodo);
+    const { done, title, category } = req.body;
+    getRepository(Category).findOneOrFail(
+      { where: { userId: req.user.id, id: category } },
+    ).then((_foundCategory) => {
+      const manager = getManager();
+      const todo = manager.create(ToDo, { done, title });
+      todo.category = _foundCategory;
+      todo.user = req.user;
+      manager.save(todo).then((savedTodo) => {
+        res.send(savedTodo);
+      });
+    }, () => {
+      res.send(404);
     });
   });
 router.route('/todos/:id')
   .all(isAuthenticated)
   .all((req, res, next) => {
     getRepository(ToDo).findOneOrFail(
-      { where: { userId: req.user.id, id: req.params.id } },
+      { where: { userId: req.user.id, id: req.params.id }, relations: ['category'] },
     ).then((_foundTodo) => {
       req.todo = _foundTodo;
       next();
@@ -34,9 +42,17 @@ router.route('/todos/:id')
   })
   .put((req, res) => {
     const foundTodo = req.todo;
-    const { title, done } = req.body;
+    const { title, done, category } = req.body;
     foundTodo.title = title;
     foundTodo.done = done;
+    getRepository(Category).findOneOrFail(
+      { where: { userId: req.user.id, id: category } },
+    ).then((_foundCategory) => {
+      foundTodo.category = _foundCategory;
+    }, () => {
+      res.send(404);
+    });
+
     getManager().save(foundTodo).then((updatedTodo) => {
       res.send(updatedTodo);
     });
