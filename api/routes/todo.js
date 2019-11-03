4ccef2af7c7a80e4fbd0529374ac9be5bbc31cf3
Andrew Yan <andrewyan@Andrews-MacBook-Pro-2.local>
2019-11-02 19:39:43 -0700

    Added Categories

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -7,15 +7,28 @@ const router = Router();
 router.route('/todos')
   .all(isAuthenticated)
   .get((req, res) => {
-    res.send(req.user.todos);
+    getRepository(ToDo).find({ 
+      where: { userId: req.user.id },
+      relations: ['category']
+    })
+    .then(todo => {
+      res.send(todo);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
   })
   .post((req, res) => {
-    const { done, title } = req.body;
+    const { done, title, category } = req.body;
     const manager = getManager();
     const todo = manager.create(ToDo, { done, title });
     todo.user = req.user;
+    todo.category = category;
     manager.save(todo).then((savedTodo) => {
       res.send(savedTodo);
+    })
+    .catch(err => {
+      res.status(400).send(err);
     });
   });
 router.route('/todos/:id')
